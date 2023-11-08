8c9f27468a1631f37dac95703cd8f712aaee68fe
jussi <10192819+jushii@users.noreply.github.com>
2023-11-08 18:37:30 +0800

    Added GainShieldEffect and Defend card

---
+++ Assets/Scripts/Data/Effects/GainShieldEffect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(menuName = "Gamejam/Effect/Gain Shield Effect", fileName = "New Gain Shield Effect")]
+    public class GainShieldEffect : EffectData
+    {
+        public int value;
+        
+        public override IEnumerator Execute(
+            RuntimeCard card,
+            RuntimeCharacter characterPlayingTheCard,
+            RuntimeCharacter playerCharacter,
+            RuntimeCharacter targetCharacter,
+            List<RuntimeCharacter> enemyCharacters)
+        {
+            // TODO: VFX
+
+            characterPlayingTheCard.properties.Get<int>(PropertyKey.SHIELD).Value += value;
+            
+            yield break;
+        }
+
+        public override string GetDescriptionText(RuntimeCard card, RuntimeCharacter playerCharacter)
+        {
+            return $"Gain {value} shield.";
+        }
+    }
+}
