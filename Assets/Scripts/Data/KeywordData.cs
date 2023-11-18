2b7d50e06444001e1b2edd92052a9db542827149
jussi <10192819+jushii@users.noreply.github.com>
2023-11-18 15:45:32 +0800

    Added KeywordLinkTooltipHandler to Nat_WIPScene

---
+++ Assets/Scripts/Data/KeywordData.cs
@@ -0,0 +1,13 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(menuName = "Gamejam/Keyword Description", fileName = "New Keyword Description")]
+    public class KeywordData : ScriptableObject
+    {
+        public string header;
+        [ResizableTextArea]
+        public string description;
+    }
+}
