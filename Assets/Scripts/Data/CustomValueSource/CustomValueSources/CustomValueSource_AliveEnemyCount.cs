c9bf28f8024b0f5642c299adb1f8cc323d97ff4c
jussi <10192819+jushii@users.noreply.github.com>
2023-11-19 14:18:22 +0800

    Card/custom value source update

---
+++ Assets/Scripts/Data/CustomValueSource/CustomValueSources/CustomValueSource_AliveEnemyCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(menuName = "Gamejam/Custom Value Source/Alive Enemy Count", fileName = "Alive Enemy Count")]
+    public class CustomValueSource_AliveEnemyCount : CustomValueSource
+    {
+        public override int GetValue(RuntimeCard card, RuntimeCharacter characterPlayingTheCard, RuntimeCharacter player, RuntimeCharacter cardTarget, List<RuntimeCharacter> enemies)
+        {
+            int value = 0;
+            
+            if (BattleManager.current != null)
+            {
+                foreach (Character enemy in BattleManager.current.enemies)
+                {
+                    Property<CharacterState> characterState = enemy.runtimeCharacter.properties.Get<CharacterState>(PropertyKey.CHARACTER_STATE);
+                    if (characterState.Value == CharacterState.ALIVE)
+                    {
+                        value++;
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+}
