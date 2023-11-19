f4fe71dc7285cf3a596e99668bba4ab6f976084a
jussi <10192819+jushii@users.noreply.github.com>
2023-11-19 18:31:17 +0800

    Fix NumberOfCardsDiscardedByThisCard

--- Assets/Scripts/Data/CustomValueSource/CustomValueSources/CustomValueSource_NumberOfCardsDiscardedByThisCard.cs
+++ Assets/Scripts/Data/CustomValueSource/CustomValueSources/CustomValueSource_NumberOfCardsDiscardedByThisCard.cs
@@ -8,6 +8,15 @@ namespace DefaultNamespace
     {
         public override int GetValue(RuntimeCard card, RuntimeCharacter characterPlayingTheCard, RuntimeCharacter player, RuntimeCharacter cardTarget, List<RuntimeCharacter> enemies)
         {
+            Property<CardState> cardState = card.properties.Get<CardState>(PropertyKey.CARD_STATE);
+
+            // If the card is still in your hand, display the number of cards in your hand that would be discarded.
+            if (cardState.Value == CardState.HAND)
+            {
+                return BattleManager.current.CardController.HandPile.Cards.Count;
+            }
+            
+            // Otherwise display the amount of cards discarded by the card currently being played.
             return characterPlayingTheCard.properties.Get<int>(PropertyKey.CARDS_DISCARDED_BY_CURRENTLY_BEING_PLAYED_CARD).Value;
         }
     }
