a857806dac993a65ae9fa58e5b262072f8d39337
NattyboyZ <iamnattyboyz@hotmail.com>
2023-11-26 23:01:38 +0700

    IntentionUI update SetIntention add playanimation boolean

--- Assets/IntentionUI.cs
+++ Assets/IntentionUI.cs
@@ -19,7 +19,7 @@ public class IntentionUI : MonoBehaviour
 
     [SerializeField] private float showDuration = 0.1f;
     
-    public IEnumerator SetIntention(List<IntentionDetail> intentDetails)
+    public IEnumerator SetIntention(List<IntentionDetail> intentDetails, bool withAnimation = true)
     {
         ClearIntention();
         foreach (var intentionDetail in intentDetails)
@@ -53,12 +53,14 @@ public class IntentionUI : MonoBehaviour
             icon.SetMultiplier(intentionDetail._multiplier);
             icon.SetSizeEffect(intentionDetail._size);
             icon.gameObject.SetActive(true);
-            icon.transform.localScale = Vector3.zero;
-            
-            yield return icon.transform.DOScale(1, showDuration);
+
+            if (withAnimation)
+            {
+                icon.transform.localScale = Vector3.zero;
+                yield return icon.transform.DOScale(1, showDuration);
+            }
         }
     }
-
 
     protected IntentionIcon CreateIntentionIcon()
     {
@@ -67,7 +69,6 @@ public class IntentionUI : MonoBehaviour
         icon.transform.localScale = new Vector3(1, 1, 1);
         return icon;
     }
-
 
     protected void ClearIntention()
     {
@@ -78,7 +79,7 @@ public class IntentionUI : MonoBehaviour
         }
         _use_intentions.Clear();
     }
-
+        
     #region TestFunction
 
 
