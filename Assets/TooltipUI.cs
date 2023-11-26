651ee2b42acbb79f1608c080e62a0b89d09938b7
NattyboyZ <iamnattyboyz@hotmail.com>
2023-11-27 00:57:23 +0700

    Update StatUI

--- Assets/TooltipUI.cs
+++ Assets/TooltipUI.cs
@@ -51,6 +51,9 @@ public class TooltipUI : MonoBehaviour
 
         _header_txt.gameObject.SetActive(false);
         _content.gameObject.SetActive(true);
+
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_content.rectTransform);
     }
 
     public void Show(string header, string text, Vector3 position, Side side)
@@ -84,7 +87,41 @@ public class TooltipUI : MonoBehaviour
 
         _header_txt.gameObject.SetActive(true);
         _content.gameObject.SetActive(true);
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_content.rectTransform);
     }
+
+    //IEnumerator ieShow(string text, Vector3 position, Side side)
+    //{
+    //    Vector2 p;
+
+    //    if (side == Side.BotRight)//Pivot TopLeft -> Graphic on Botright.
+    //    {
+    //        p = new Vector2(0, 1);
+    //    }
+    //    else if (side == Side.BotLeft)//Pivot TopRight  -> Graphic on Botleft.
+    //    {
+    //        p = new Vector2(1, 1);
+    //    }
+    //    else if (side == Side.TopRight)//Pivot BotLeft  -> Graphic on Topright.
+    //    {
+    //        p = new Vector2(0, 0);
+    //    }
+    //    else//Pivot BotRight -> Graphic on Topleft.
+    //    {
+    //        p = new Vector2(1, 0);
+    //    }
+
+    //    _canvas.position = position;
+    //    _canvas.pivot = p;
+    //    _content.rectTransform.anchorMin = p;
+    //    _content.rectTransform.anchorMax = p;
+    //    _content.rectTransform.pivot = p;
+    //    _tooltip_txt.text = text;
+        
+    //    _header_txt.gameObject.SetActive(false);
+    //    _content.gameObject.SetActive(true);
+    //}
 
     /// <summary>
     /// Hide tooltip
