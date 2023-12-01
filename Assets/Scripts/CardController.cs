ca1142c1bb1670fa30ccc8a0cd7f58dc16bb9ba7
Khami <napakamee@outlook.com>
2023-12-02 02:20:00 +0700

    Polish card controller

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -87,7 +87,7 @@ public class CardController : MonoBehaviour
         {
             DeckPile.AddCard(_recycledCard, true, Vector3.zero);
                 
-            yield return drawDelay;
+            yield return null;
         }
         DeckPile.Shuffle();
     }
